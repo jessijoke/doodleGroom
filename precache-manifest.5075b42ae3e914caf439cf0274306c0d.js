2a38bb482c9b3c726e965a52503a81958f8a9f31
jessica bradham <jessijoke@yahoo.com>
2020-05-06 00:26:09 -0400

    First commit

---
+++ precache-manifest.5075b42ae3e914caf439cf0274306c0d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae958e76dbec04662bdf8c44647c9290",
+    "url": "/JessicaJoy/groom/index.html"
+  },
+  {
+    "revision": "49d390f582b403b75b09",
+    "url": "/JessicaJoy/groom/static/css/2.036b41f9.chunk.css"
+  },
+  {
+    "revision": "834e9de26740057d7975",
+    "url": "/JessicaJoy/groom/static/css/main.647489b3.chunk.css"
+  },
+  {
+    "revision": "49d390f582b403b75b09",
+    "url": "/JessicaJoy/groom/static/js/2.f9f6171b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/JessicaJoy/groom/static/js/2.f9f6171b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "834e9de26740057d7975",
+    "url": "/JessicaJoy/groom/static/js/main.2acd3cd8.chunk.js"
+  },
+  {
+    "revision": "6e73da5c494821bfbd66",
+    "url": "/JessicaJoy/groom/static/js/runtime-main.996a4ae3.js"
+  },
+  {
+    "revision": "46709149e177010e9dfafc646058dd72",
+    "url": "/JessicaJoy/groom/static/media/doodle.46709149.jpg"
+  },
+  {
+    "revision": "1d06864cf4b548772daafda92958d56d",
+    "url": "/JessicaJoy/groom/static/media/logo.1d06864c.svg"
+  }
+]);
